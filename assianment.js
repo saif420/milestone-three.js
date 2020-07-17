83ba4ba250c4864adf58d30785395c662643f8ee
saif420 <saifmahmud42069@gmail.com>
2020-07-17 07:26:25 +0600

    feet to mile

---
+++ assianment.js
@@ -0,0 +1,25 @@
+// feetToMile
+
+function feetToMile(feet){
+    var mile = feet * 0.000189394;
+    return mile; 
+}
+
+// feetToMile
+
+// woodCalculator
+
+
+
+
+
+
+
+// woodCalculator
+
+// brickCalculator
+// brickCalculator
+
+
+// tinyFriend
+// tinyFriend
