e2721523425b8f5ff433daaca4a486e9bc78a517
saif420 <saifmahmud42069@gmail.com>
2020-07-17 08:45:56 +0600

    add tiny friend

--- assianment.js
+++ assianment.js
@@ -1,25 +1,36 @@
 // feetToMile
-
 function feetToMile(feet){
     var mile = feet * 0.000189394;
     return mile; 
 }
-
 // feetToMile
 
 // woodCalculator
-
-
-
-
-
-
-
+var result = woodCalculator(12, 12, 15);
+function woodCalculator(chair, table, bad){
+    var chairCount = chair * 1;
+    var tableCount = table * 3;
+    var badCount = bad * 5;
+    var totalKhat = (chairCount + tableCount + badCount);
+    return totalKhat;
+}
 // woodCalculator
 
 // brickCalculator
+
+
 // brickCalculator
 
 
 // tinyFriend
+function tinyFriend(names){
+    let smallFriend = names[0];
+    for(let i = 0; i < names.length; i++){
+        let newName = names[i];
+        if(newName < smallFriend){
+            smallFriend = newName;
+        }
+    }
+    return smallFriend;
+}
 // tinyFriend
